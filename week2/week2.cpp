b3160ceea4c4e13adda8b59227f44ede13bb540a
HappyGab <105558236+HappyGab@users.noreply.github.com>
2023-01-25 23:18:23 -0500

    Finished constructors

--- week2/week2.cpp
+++ week2/week2.cpp
@@ -1,8 +1,34 @@
 
 #include<iostream>
+using namespace std;
+
+namespace CST8219 {
+	class Car;
+}
+
+class CST8219::Car{
+
+	int numWheels;
+	int numDoors;
+
+public:
+	Car() {
+
+	}
+
+	Car(int w) {
+
+	}
+
+	Car(int w, int d) {
+
+		numWheels = w;
+		numDoors = d;
+	}
+};
 
 int main(int argc, char **argv)
 {
-   std::cout << "Hello World!" << std::endl;
+   cout << "Hello World!" << endl;
    return 0;
 }
